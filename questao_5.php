ce27e626e58c290622b6e3cf3e5cb29ea79e2e39
Rafael Souza Ambrosi <leafar2010@gmail.com>
2023-04-10 12:36:39 -0300

    Update questao_5.php

--- questao_5.php
+++ questao_5.php
@@ -1,10 +1,14 @@
 <?php
 //Resposta da questão 5, programa que inverte os caracteres das strings
 
-  $string = "Olá mundo!"; // Define a string a ser invertida
-  $invertedString = ""; // Define uma variável vazia para armazenar a string invertida
-  for ($i = strlen($string) - 1; $i >= 0; $i--) { // Percorre cada caractere da string de trás para frente
-    $invertedString .= $string[$i]; // Adiciona o caractere atual no início da string invertida
-  }
-  echo $invertedString; // Imprime a string invertida
+$str = "exemplo"; // string original
+$newStr = ""; // nova string, vazia no início
+
+// percorre a string original de trás para frente
+for ($i = strlen($str) - 1; $i >= 0; $i--) {
+  $newStr .= $str[$i]; // adiciona o caractere atual na nova string
+}
+
+echo $newStr; // imprime a nova string invertida: "olpmexe"
+
 ?>
